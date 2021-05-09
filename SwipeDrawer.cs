64d0fb7c4549a807e64beb149e2ea095aada3122
HarshilBhattBossy <81522458+HarshilBhattBossy@users.noreply.github.com>
2021-05-09 20:58:42 +0400

    Add files via upload

---
+++ SwipeDrawer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDrawer : MonoBehaviour
+{
+
+    private LineRenderer lineRender;
+    private float zOffset = 10;
+    private void Awake()
+    {
+        lineRender = GetComponent<LineRenderer>();
+        Algo.OnSwipe += SwipeDetect;
+    }
+
+    private void SwipeDetect(SwipeData data)
+    {
+
+        Vector3[] positon = new Vector3[2];
+        positon[0] = Camera.main.ScreenToWorldPoint(new Vector3(data.StartPos.x, data.StartPos.y, zOffset));
+        positon[1] = Camera.main.ScreenToWorldPoint(new Vector3(data.EndPos.x, data.EndPos.y, zOffset));
+        lineRender.positionCount = 2;
+        lineRender.SetPositions(positon);
+
+    }
+
+}
---
+++ SwipeLogger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeLogger : MonoBehaviour
+{
+    private float nextActionTime = 0.0f;
+    private float DeleteTime = 0.0f;
+    public float period = 8f;
+    public float period02 = 11f;
+    Queue<GameObject> myStack = new Queue<GameObject>();
+    public GameObject Anime;
+
+    private void Start()
+    {
+        DeleteTime = period02;
+    }
+    private void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+
+            Debug.Log("Soccer Ball clicked");
+            GameObject gameObject = Instantiate(Anime, new Vector3(0, 0, 1), Quaternion.identity);
+            gameObject.transform.Rotate(0, -180, 0);
+            myStack.Enqueue(gameObject);
+            nextActionTime += period;
+
+        }
+        if (myStack.Count != 0 && DeleteTime < Time.time)
+        {
+            Destroy(myStack.Peek());
+            myStack.Dequeue();
+            DeleteTime += period02;
+        }
+    }
+    private void Awake()
+    {
+        Algo.OnSwipe += SwipeDetector;
+    }
+
+    private void SwipeDetector(SwipeData data)
+    {
+        Debug.Log("Swiping" + data.Direction);
+    }
+}
